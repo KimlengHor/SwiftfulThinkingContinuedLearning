fb3ab23dd30bf22259990cc77b4f38378499a155
Kimleng <kimlenghor2006@gmail.com>
2023-03-30 13:32:06 -0500

    Initial Commit

---
+++ SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingContinuedLearningApp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Kimleng Hor on 3/30/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingContinuedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
